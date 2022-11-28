5901816311cf675a3296a94b8d1a12499026532f
gustavocadev <churaalaygustavo11@gmail.com>
2022-11-28 10:44:55 -0500

    chore: update to remix@1.7.6

--- app/components/quiosco/Product.tsx
+++ app/components/quiosco/Product.tsx
@@ -22,8 +22,7 @@ const Product = ({ product }: Props) => {
         <figcaption className="p-5">
           <h3 className="text-2xl font-bold">{name}</h3>
           <p className="mt-5 text-4xl font-black text-amber-500">
-            {/* {formatMoney(price, "USD")} */}
-            {price}
+            {/* {formatMoney(price, "USD")} */}${price}
           </p>
           <button
             className="w-full p-3 mt-3 font-bold text-white uppercase bg-indigo-600 rounded hover:bg-indigo-800"
